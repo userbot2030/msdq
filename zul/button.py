5aae95c63a64354278eafd879f5733162f457d6a
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-13 01:13:13 +0700

    Update button.py

--- zul/button.py
+++ zul/button.py
@@ -4,44 +4,44 @@ from msdq import BOT_USERNAME
 BMANAGE = InlineKeyboardMarkup(
                 [
                  [
-                    InlineKeyboardButton(text="💁🏻‍♂Perintah Dasar", callback_data="zel_dasar"),
-                    InlineKeyboardButton(text="Lanjutan🙋🏻‍♂", callback_data="zel_lanjut"),
+                    InlineKeyboardButton(text="💁🏻‍♂Perintah Dasar", callback_data="zul_dasar"),
+                    InlineKeyboardButton(text="Lanjutan🙋🏻‍♂", callback_data="zul_lanjut"),
                  ],
                  [
-                    InlineKeyboardButton(text="🕵🏻Ahli", callback_data="zel_ahli"),
-                    InlineKeyboardButton(text="Panduan Pro💆🏻‍♂", callback_data="zel_pro"),
+                    InlineKeyboardButton(text="🕵🏻Ahli", callback_data="zul_ahli"),
+                    InlineKeyboardButton(text="Panduan Pro💆🏻‍♂", callback_data="zul_pro"),
                  ],
                  [
                     InlineKeyboardButton(text="➕ Panduan Lengkap ➕", url=f"http://t.me/{BOT_USERNAME}?start=help"),
                  ],
                  [
-                    InlineKeyboardButton(text="🔙 Kembali", callback_data="zel_back"),
+                    InlineKeyboardButton(text="🔙 Kembali", callback_data="zul_back"),
                  ]
                 ])
 BBMANAGE = InlineKeyboardMarkup(
                 [
                  [
-                    InlineKeyboardButton(text="🔙 Kembali", callback_data="zel_manage"),
+                    InlineKeyboardButton(text="🔙 Kembali", callback_data="zul_manage"),
                  ]
                 ])
 BMUSIC = InlineKeyboardMarkup(
                 [
                  [
-                    InlineKeyboardButton(text="ᴘᴇʀɪɴᴛᴀʜ ᴀᴅᴍɪɴ", callback_data="zel_admin"),
-                    InlineKeyboardButton(text="ᴘᴇʀɪɴᴛᴀʜ ʙᴏᴛ", callback_data="zel_bot"),
+                    InlineKeyboardButton(text="ᴘᴇʀɪɴᴛᴀʜ ᴀᴅᴍɪɴ", callback_data="zul_admin"),
+                    InlineKeyboardButton(text="ᴘᴇʀɪɴᴛᴀʜ ʙᴏᴛ", callback_data="zul_bot"),
                  ],
                  [
-                    InlineKeyboardButton(text="ᴘᴇʀɪɴᴛᴀʜ ᴘʟᴀʏ", callback_data="zel_play"),
-                    InlineKeyboardButton(text="ᴘᴇʀɪɴᴛᴀʜ ᴇxsᴛʀᴀ", callback_data="zel_extra"),
+                    InlineKeyboardButton(text="ᴘᴇʀɪɴᴛᴀʜ ᴘʟᴀʏ", callback_data="zul_play"),
+                    InlineKeyboardButton(text="ᴘᴇʀɪɴᴛᴀʜ ᴇxsᴛʀᴀ", callback_data="zul_extra"),
                  ],
                  [
-                    InlineKeyboardButton(text="«", callback_data="zel_back"),
+                    InlineKeyboardButton(text="«", callback_data="zul_back"),
                  ]
                 ])
 BBMUSIC = InlineKeyboardMarkup(
                 [
                  [
-                    InlineKeyboardButton(text="🔙 Kembali", callback_data="zel_music"),
+                    InlineKeyboardButton(text="🔙 Kembali", callback_data="zul_music"),
                  ]
                 ])
 BJASA = InlineKeyboardMarkup(
@@ -50,7 +50,7 @@ BJASA = InlineKeyboardMarkup(
                      InlineKeyboardButton("Admin", url="https://t.me/msdqqq")
                  ],
                  [
-                    InlineKeyboardButton(text="«", callback_data="zel_back"),
+                    InlineKeyboardButton(text="«", callback_data="zul_back"),
                  ]
               ])
     
