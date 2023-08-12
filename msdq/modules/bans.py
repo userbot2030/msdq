79ab3ad9156e4bb127540e6e60c3e8778906d942
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-12 22:16:14 +0700

    Update bans.py

--- msdq/modules/bans.py
+++ msdq/modules/bans.py
@@ -1,19 +1,3 @@
-#  ZeldrisRobot
-#  Copyright (C) 2017-2019, Paul Larsen
-#  Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 import html
@@ -24,11 +8,11 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, CallbackContext
 from telegram.utils.helpers import mention_html
 
-from zeldris import dispatcher, LOGGER
-from zeldris.modules.disable import DisableAbleCommandHandler
-from zeldris.modules.helper_funcs.admin_rights import user_can_ban
-from zeldris.modules.helper_funcs.alternate import typing_action
-from zeldris.modules.helper_funcs.chat_status import (
+from msdq import dispatcher, LOGGER
+from msdq.modules.disable import DisableAbleCommandHandler
+from msdq.modules.helper_funcs.admin_rights import user_can_ban
+from msdq.modules.helper_funcs.alternate import typing_action
+from msdq.modules.helper_funcs.chat_status import (
     bot_admin,
     user_admin,
     is_user_ban_protected,
@@ -37,9 +21,9 @@ from zeldris.modules.helper_funcs.chat_status import (
     is_user_in_chat,
     can_delete,
 )
-from zeldris.modules.helper_funcs.extraction import extract_user_and_text
-from zeldris.modules.helper_funcs.string_handling import extract_time
-from zeldris.modules.log_channel import loggable
+from msdq.modules.helper_funcs.extraction import extract_user_and_text
+from msdq.modules.helper_funcs.string_handling import extract_time
+from msdq.modules.log_channel import loggable
 
 
 @bot_admin
