2b8ad19087731e983f4d471e79db7dc80539e6a5
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-12 23:31:03 +0700

    Update users.py

--- msdq/modules/users.py
+++ msdq/modules/users.py
@@ -1,19 +1,4 @@
-#  ZeldrisRobot
-#  Copyright (C) 2017-2019, Paul Larsen
-#  Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 
 import contextlib
@@ -25,9 +10,9 @@ from telegram import TelegramError, Update
 from telegram.error import BadRequest
 from telegram.ext import MessageHandler, Filters, CommandHandler, CallbackContext
 
-import zeldris.modules.no_sql.users_db as users_db
-from zeldris import dispatcher, OWNER_ID, LOGGER
-from zeldris.modules.helper_funcs.filters import CustomFilters
+import msdq.modules.no_sql.users_db as users_db
+from msdq import dispatcher, OWNER_ID, LOGGER
+from msdq.modules.helper_funcs.filters import CustomFilters
 
 USERS_GROUP = 4
 CHAT_GROUP = 10
