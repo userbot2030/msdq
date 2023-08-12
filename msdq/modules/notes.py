49195de3624c9f0ea1b75442413df56861f3af13
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-12 23:03:27 +0700

    Update notes.py

--- msdq/modules/notes.py
+++ msdq/modules/notes.py
@@ -1,19 +1,4 @@
-#  ZeldrisRobot
-#  Copyright (C) 2017-2019, Paul Larsen
-#  Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 
 import ast
@@ -40,15 +25,15 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html
 
-import zeldris.modules.sql.notes_sql as sql
-from zeldris import dispatcher, MESSAGE_DUMP, LOGGER
-from zeldris.modules.connection import connected
-from zeldris.modules.disable import DisableAbleCommandHandler
-from zeldris.modules.helper_funcs.alternate import typing_action
-from zeldris.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
-from zeldris.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from zeldris.modules.helper_funcs.msg_types import get_note_type
-from zeldris.modules.helper_funcs.string_handling import (
+import msdq.modules.sql.notes_sql as sql
+from msdq import dispatcher, MESSAGE_DUMP, LOGGER
+from msdq.modules.connection import connected
+from msdq.modules.disable import DisableAbleCommandHandler
+from msdq.modules.helper_funcs.alternate import typing_action
+from msdq.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
+from msdq.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from msdq.modules.helper_funcs.msg_types import get_note_type
+from msdq.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_to_html,
 )
