8b3da7b10252380b2e2c09adb8e6c0c2764a2f5e
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-12 22:59:28 +0700

    Update misc.py

--- msdq/modules/misc.py
+++ msdq/modules/misc.py
@@ -1,19 +1,4 @@
-#  ZeldrisRobot
-#  Copyright (C) 2017-2019, Paul Larsen
-#  Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 
 import contextlib
@@ -44,7 +29,7 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, CallbackContext
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from zeldris import (
+from msdq import (
     dispatcher,
     OWNER_ID,
     DEV_USERS,
@@ -53,11 +38,11 @@ from zeldris import (
     WALL_API,
     spamwtc,
 )
-from zeldris.__main__ import STATS, USER_INFO, GDPR
-from zeldris.modules.disable import DisableAbleCommandHandler
-from zeldris.modules.helper_funcs.alternate import typing_action, send_action
-from zeldris.modules.helper_funcs.extraction import extract_user
-from zeldris.modules.helper_funcs.filters import CustomFilters
+from msdq.__main__ import STATS, USER_INFO, GDPR
+from msdq.modules.disable import DisableAbleCommandHandler
+from msdq.modules.helper_funcs.alternate import typing_action, send_action
+from msdq.modules.helper_funcs.extraction import extract_user
+from msdq.modules.helper_funcs.filters import CustomFilters
 
 
 @typing_action
