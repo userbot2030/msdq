a1ba2d5dcec2aefb70d4f543226c538ffb57a59f
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-13 00:54:28 +0700

    Update users_db.py

--- msdq/modules/no_sql/users_db.py
+++ msdq/modules/no_sql/users_db.py
@@ -1,24 +1,9 @@
-#  ZeldrisRobot
-#  Copyright (C) 2017-2019, Paul Larsen
-#  Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 """Users Database."""
 
-from zeldris import dispatcher
-from zeldris.modules.no_sql import get_collection
+from msdq import dispatcher
+from msdq.modules.no_sql import get_collection
 
 USERS_DB = get_collection("USERS")
 CHATS_DB = get_collection("CHATS")
