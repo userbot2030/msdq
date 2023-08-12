47fb4a86ceac9fa200d6e9fb90a10e2bb7140fe4
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-12 22:23:03 +0700

    Update dbcleanup.py

--- msdq/modules/dbcleanup.py
+++ msdq/modules/dbcleanup.py
@@ -1,19 +1,4 @@
-#  ZeldrisRobot
-#  Copyright (C) 2017-2019, Paul Larsen
-#  Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 
 import contextlib
@@ -23,9 +8,9 @@ from telegram import Bot, Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, CallbackQueryHandler, Filters
 
-import zeldris.modules.no_sql.gban_db as db
-import zeldris.modules.no_sql.users_db as user_db
-from zeldris import dispatcher, DEV_USERS
+import msdq.modules.no_sql.gban_db as db
+import msdq.modules.no_sql.users_db as user_db
+from msdq import dispatcher, DEV_USERS
 
 
 def get_invalid_chats(bot: Bot, update: Update, remove: bool = False):
