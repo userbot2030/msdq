d0455000b709a60a55c820ece4e18f877e76ec91
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-12 22:24:41 +0700

    Update disable.py

--- msdq/modules/disable.py
+++ msdq/modules/disable.py
@@ -1,19 +1,4 @@
-#  ZeldrisRobot
-#  Copyright (C) 2017-2019, Paul Larsen
-#  Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 
 import importlib
@@ -30,21 +15,21 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import escape_markdown
 
-from zeldris import dispatcher
-from zeldris.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
-from zeldris.modules.helper_funcs.misc import is_module_loaded
+from msdq import dispatcher
+from msdq.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
+from msdq.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from zeldris.modules.helper_funcs.chat_status import (
+    from msdq.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from zeldris.modules.sql import disable_sql as sql
+    from msdq.modules.sql import disable_sql as sql
 
     DISABLE_CMDS = []
     DISABLE_OTHER = []
