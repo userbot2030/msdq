22bfb568870fec4e8891639ed0bc8aa855bd808c
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-12 22:27:09 +0700

    Update eval.py

--- msdq/modules/eval.py
+++ msdq/modules/eval.py
@@ -1,19 +1,4 @@
-#  ZeldrisRobot
-#  Copyright (C) 2017-2019, Paul Larsen
-#  Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 
 import ast
@@ -27,8 +12,8 @@ from contextlib import redirect_stdout, suppress
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 
-from zeldris import LOGGER, TOKEN, dispatcher
-from zeldris.modules.helper_funcs.chat_status import dev_plus
+from msdq import LOGGER, TOKEN, dispatcher
+from msdq.modules.helper_funcs.chat_status import dev_plus
 
 namespaces = {}
 
@@ -90,7 +75,7 @@ def do(func, update, context):  # skipcq
 
     os.chdir(os.getcwd())
     with open(
-        os.path.join(os.getcwd(), "zeldris/modules/helper_funcs/temp.txt"),
+        os.path.join(os.getcwd(), "msdq/modules/helper_funcs/temp.txt"),
         "w",
     ) as temp:
         temp.write(body)
