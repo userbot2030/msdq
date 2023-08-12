85e21efe07d1f704fcddc9a7840d5c9a8ace2ae3
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-13 00:52:06 +0700

    Update __init__.py

--- msdq/modules/no_sql/__init__.py
+++ msdq/modules/no_sql/__init__.py
@@ -1,19 +1,4 @@
-#  ZeldrisRobot
-#  Copyright (C) 2017-2019, Paul Larsen
-#  Copyright (C) 2022, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 
 """MongoDB Database."""
 
@@ -21,7 +6,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from odmantic import AIOEngine
 from pymongo import MongoClient, collection
 
-from zeldris import MONGO_DB, MONGO_PORT, MONGO_URI
+from msdq import MONGO_DB, MONGO_PORT, MONGO_URI
 
 # MongoDB Client
 mongodb = MongoClient(MONGO_URI, MONGO_PORT)[MONGO_DB]
@@ -29,7 +14,7 @@ motor = AsyncIOMotorClient(MONGO_URI)
 db = motor[MONGO_DB]
 engine = AIOEngine(motor, MONGO_DB)
 DB_CLIENT = MongoClient(MONGO_URI)
-_DB = DB_CLIENT["Zeldris"]
+_DB = DB_CLIENT["MSDQROBOT"]
 
 
 def get_collection(name: str) -> collection:
