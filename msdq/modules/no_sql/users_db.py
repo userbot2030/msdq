4506c68615f829836a728a15bf8098986f552d50
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-09-23 16:09:44 +0700

    Update users_db.py

--- msdq/modules/no_sql/users_db.py
+++ msdq/modules/no_sql/users_db.py
@@ -10,11 +10,15 @@ CHATS_DB = get_collection("CHATS")
 CHAT_MEMBERS_DB = get_collection("CHAT_MEMBERS")
 
 
-async def cek(user_id):
+
+
+
+
+def cek(user_id):
     ada = USERS_DB.find_one({'_id': user_id})
     return bool(ada)
 
-async def tambah(user_id):
+def tambah(user_id):
     USERS_DB.insert_one({'_id': user_id})
     return
 
