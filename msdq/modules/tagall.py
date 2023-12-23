f4c8231ac200d42ecac109533cd948f9611e2e3a
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-23 23:48:14 +0700

    NT tagall.py

--- msdq/modules/tagall.py
+++ msdq/modules/tagall.py
@@ -62,7 +62,7 @@ async def mentionall(event):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"🔴 [{usr.first_name}](tg://user?id={usr.id})\n"
+        usrtxt += f"🏓 [{usr.first_name}](tg://user?id={usr.id})\n"
         if usrnum == 5:
             if mode == "text_on_cmd":
                 txt = f"{msg}\n\n{usrtxt}"
