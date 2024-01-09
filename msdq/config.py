5b428b782d8b2f208e7544c1fe41e8882b972995
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2024-01-09 19:58:51 +0700

    E config.py

--- msdq/config.py
+++ msdq/config.py
@@ -5,7 +5,7 @@ class Config(object):
     # WAJIB
     TOKEN = ""
     OWNER_ID = (
-        "6024180996"
+        "1825139822"
     )
     OWNER_USERNAME = "foundermidnight"
     API_HASH = ""
@@ -27,17 +27,17 @@ class Config(object):
 
     # OPTIONAL
     DEV_USERS = (
-        [6024180996]
+        [1825139822]
     )  # List of id's (not usernames) for users which have sudo access to the bot.
     SUPPORT_USERS = (
         [6024180996]
     )  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     WHITELIST_USERS = (
-        [6024180996]
+        [1825139822 6024180996 ]
     )  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     WHITELIST_CHATS = []
     BLACKLIST_CHATS = []
-    DONATION_LINK = "https://t.me/Foundermidnight"
+    DONATION_LINK = "https://t.me/adiramontok"
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
@@ -45,7 +45,7 @@ class Config(object):
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
     BAN_STICKER = None  # banhammer marie sticker
     ALLOW_EXCL = False  # DEPRECATED, USE BELOW INSTEAD! Allow ! commands as well as /
-    CUSTOM_CMD = False  # Set to ('/', '!') or whatever to enable it, like ALLOW_EXCL but with more custom handler!
+    CUSTOM_CMD = False  # Set to ('/', 'None') or whatever to enable it, like ALLOW_EXCL but with more custom handler!
     API_OPENWEATHER = None  # OpenWeather API
     SPAMWATCH_API = "UvVoGCq~EZaGZnIXubnELN_sDNP7fxahvT9I2eIfdXniJ6qzyj27Jbn3s18cmS3P"
     WALL_API = None
